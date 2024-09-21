fb9f5d20070fdcb037837157df453a2643414f57
Nikitoshka16 <nscirikitos@yandex.ru>
2024-09-21 20:46:41 +0500

    вывод треков на фронте

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
-    <ul>
-      <li v-for="musician in musicians" :key="musician.id">
-        <strong>Label Name:</strong> {{ musician.labelName }}<br />
-        <strong>Label Country:</strong> {{ musician.labelCountry }}
-      </li>
-    </ul>
+    <div v-for="song in songs" :key="song.id">
+      <strong> {{ song.musician.musiciannickname }} - {{ song.namesong }} {{ song.songduration }}</strong>
+    </div>
   </div>
 </template>
 
@@ -15,18 +12,18 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      musicians: [],
+      songs: [],
     };
   },
   mounted() {
-    this.HelloWorld();
+    this.GetAllSongs();
   },
   methods: {
-    async HelloWorld(){
+    async GetAllSongs(){
       try {
-          const response = await axios.get('http://localhost:8000/api/musicians/');
-          console.log(response.data.musicians)
-          this.musicians = response.data.musicians
+          const response = await axios.get('http://localhost:8000/api/allsongs/');
+          console.log(response.data.songs)
+          this.songs = response.data.songs
       }catch (e){
           console.log(e)
       }
